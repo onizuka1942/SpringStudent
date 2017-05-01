2784b1bc90ee2fb55c0dd6d5720e6888d02a441b
gto <blade1942@hotmail.fr>
2017-05-01 23:20:09 +0200

    third commit

--- gitTest/src/com/test/git/HelloGit.java
+++ gitTest/src/com/test/git/HelloGit.java
@@ -3,6 +3,8 @@ package com.test.git;
 public class HelloGit {
     public static void main( String[] args ) {
         // commentaire
+        // un autre commentaire
+
         System.out.println( "hello" );
     }
 
